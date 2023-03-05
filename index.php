600ce74063a840ddbfb3c2ddf61b28f82d811b13
harry-awanda <harrywanda0704@gmail.com>
2023-03-05 10:03:52 +0700

    fresh template for exam

--- index.php
+++ index.php
@@ -1,49 +1,69 @@
 <?php 
-include 'header.php';
+include 'header.php'; 
 
 ?>
-<!-- Page Heading -->
-<div class="d-sm-flex align-items-center justify-content-between mb-4">
-  <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>  
-</div>
-
-<!-- Content Row -->
-<div class="row">
-  <!-- Earnings (Monthly) Card Example -->
-  <div class="col-xl-3 col-md-6 mb-4">
-    <div class="card border-left-primary shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-          <div class="col mr-2">
-            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-            Total Transaksi</div>
-            <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
-          </div>
-            <div class="col-auto">
-            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <!-- Earnings (Monthly) Card Example -->
-  <div class="col-xl-3 col-md-6 mb-4">
-    <div class="card border-left-success shadow h-100 py-2">
-      <div class="card-body">
-        <div class="row no-gutters align-items-center">
-          <div class="col mr-2">
-            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-            Total Uang</div>
-            <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
-          </div>
-          <div class="col-auto">
-            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+<!-- button triger -->
+<button type="button" class="btn btn-primary mb-3">Tambah Data</button>
+<!-- button triger -->
+<!-- DataTales Example -->
+<div class="card shadow mb-4">
+	<div class="card-header py-3">
+			<h6 class="m-0 font-weight-bold text-primary">Data Angkatan</h6>
+	</div>
+	<div class="card-body">
+		<div class="table-responsive">
+			<!-- Tabel data untuk menanmpilkan data hasil query database beserta tombol edit dan delete-->
+		</div>
+	</div>
 </div>
 
 <?php include 'footer.php'; ?>
+
+<!-- Modal untuk tambah data-->
+<div class="modal fade" id="" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+				
+			</div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal untuk edit data-->
+<div class="modal fade" id="" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" id="">
+				<!-- form edit data dipisah ke dalam file view.php -->
+			</div>
+    </div>
+  </div>
+</div>
+
+<script>
+//script ajax
+</script>
+
+<?php 
+//Proses tambah data ke dalam tabel database
+
+
+//Proses hapus data pada tabel database
+
+
+//Proses update data pada tabel database
+
+?>
